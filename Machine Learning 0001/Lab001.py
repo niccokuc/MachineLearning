5ae57a584c93117c9fc02ad05e348d3405d0cf19
Nermin Kuc <nerminkuc74@gmail.com>
2020-01-09 16:04:54 +1100

    Add files via upload

--- Machine Learning 0001/Lab001.py
+++ Machine Learning 0001/Lab001.py
@@ -22,7 +22,7 @@ print(dataset_summary)
 cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 cdf.head(9)
 
-#we can plot each of these fearues:
+#we can plot each of these features:
 viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
 viz.hist()
 plt.show()
@@ -37,5 +37,18 @@ plt.show()
 plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
+#thats the way the cookie crumbles
 plt.show()
 
+# do the same plot but this time engine-size vs emissions
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='red')
+plt.xlabel("CYLINDERS")
+plt.ylabel("Emission")
+#thats the way the cookie crumbles
+plt.show()
+
+# https://docs.scipy.org/doc/numpy-1.15.1/user/basics.creation.html
+# https://docs.scipy.org/doc/numpy-1.15.1/reference/generated/numpy.random.rand.html
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk] #80% - TRUE
+test = cdf[~msk] #20% - FALSE
